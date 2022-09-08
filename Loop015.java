082248814959693d462642d49b363e803a9cfa77
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-08 22:48:32 +0530

    change count vowels to display vowels & consonants

--- Loop015.java
+++ Loop015.java
@@ -7,21 +7,21 @@ public class Loop015 {
 	public static void main(String[] args) {
 	
 		Scanner input = new Scanner(System.in);
-		int count_w=0;
-		int count_c=0;
+		String w=0;
+		String c=0;
 		
 			System.out.println("Enter a string: ");
 			String num =input.nextLine();
 //			System.out.println(num.length());
 			for(int i=0; i<= num.length()-1; i++){
 				if(num.charAt(i)=='a'||num.charAt(i)=='A')
-					count_w++;
+					w+=num.charAt(i);
 				else 
-					count_c++;
+					w+=num.charAt(i);
 				
 			}	
-			System.out.println("Vowels : "+count_w);
-			System.out.println("Consonent : "+count_c);
+			System.out.println("Vowels : "+w);
+			System.out.println("Consonent : "+c);
 			
 			    
 	    input.close();
