0b5e76ac490f89410cb053c35116b34bf76badb6
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-08 23:11:52 +0530

    edit code for print sum of series

--- Loop012.java
+++ Loop012.java
@@ -1,26 +1,20 @@
 package com.looping.bll;
 import java.util.Scanner;
-
-		/* Write a program to calculate the sum of following series where n is input by user. 
-			1 + 1/2 + 1/3 + 1/4 + 1/5 +…………1/n */
+	/* Write a program to calculate the sum of following series where n is input by user. 
+	1 + 1/2 + 1/3 + 1/4 + 1/5 +â€¦â€¦â€¦â€¦1/n */
 		 
 public class Loop012 {
 
 	public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
-	 
-    
-    
+	
         System.out.print("Enter a number : ");
         int number = input.nextInt();
-        
+        double sum=0;
         for(int i=1; i<=number; i++){
-        	if(i==1)
-        		System.out.print("1 + ");
-        	else
-        		System.out.print("1/"+i+" + ");
+        	sum=sum+(1.0/i);
         }
-        
+        System.out.println("Sum is : "+sum);
 
 	    input.close();
 	}
