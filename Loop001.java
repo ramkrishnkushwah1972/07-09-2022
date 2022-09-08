d0fa1d59741f567f2bd0848571a54f58eb9a7749
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-08 17:12:15 +0530

    print numbers from 1 to 10

---
+++ Loop001.java
@@ -0,0 +1,14 @@
+package com.looping.bll;
+
+import java.util.Scanner;
+//	Write a program to print numbers from 1 to 10.
+public class Loop001 {
+
+	public static void main(String[] args) {		
+		
+		for(int i=1; i<=10; i++){
+			System.out.println(i);
+		}
+	}
+}
+
