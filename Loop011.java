92e4aa20e5b0aad75a0fbcac765ffaa32e6b35d2
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-08 17:30:23 +0530

    largest and smallest numbers

---
+++ Loop011.java
@@ -0,0 +1,42 @@
+package com.looping.bll;
+import java.util.Scanner;
+
+		/*Program to enter the numbers till the user wants and at the end 
+		 the program should display the largest and smallest numbers entered.*/ 
+		 
+public class Loop011 {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	 
+    int number;
+    int max = Integer.MIN_VALUE;  
+    int min = Integer.MAX_VALUE;  
+    char ch;
+
+    do
+    {
+        System.out.print("Enter the number ");
+        number = input.nextInt();
+
+        if(number > max)
+        {
+            max = number;
+        }
+
+        if(number < min)
+        {
+            min = number;
+        }
+
+        System.out.print("Do you want to continue y/n? ");
+        ch = input.next().charAt(0);
+
+    	}while(ch=='y' || ch == 'Y');
+
+    	System.out.println("Largest number: " + max);
+    	System.out.println("Smallest number: " + min);			    
+	    input.close();
+	}
+}
+
